4f12a23ecbe0d1b813305b5c514f688db80c1e00
GeraldRyan <gerald.ryan40@yahoo.com>
2020-12-03 21:47:47 -0600

    exercise 3 completed

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -9,7 +9,29 @@
 
 int main(void)
 {
+    int ecode;
     // Your code here
+    pid_t cpid;
+    int f = fork();
+    cpid = wait(&ecode);
+    if (f > 0)
+    { // runs code that is for parent only
+        printf("I am the parent\n");
+    }
+    else if (f == 0)
+    { // runs code that is for child only // this will always run first since wait is called..i think
+        printf("I am the child\n");
+        return 100; // ints are between 0 and 255
+    }
+    // else {
+    //     cpid = wait(NULL);
+    // }
+    printf("Parent PID = %d\n", getpid());
+    printf("Child PID = %d\n", cpid);
+    printf("this is what child ended with %d\n", ecode);
+
+    if (WIFEXITED(ecode))
+        printf("Exit status: %d\n", WEXITSTATUS(ecode));
 
     return 0;
 }
