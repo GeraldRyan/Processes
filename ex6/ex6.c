0ff06c856ae65fda6c9810bfe019882aabe479aa
GeraldRyan <gerald.ryan40@yahoo.com>
2020-12-04 23:06:25 -0600

    working with clock. not sure it works on windows

--- ex6/ex6.c
+++ ex6/ex6.c
@@ -15,12 +15,23 @@ and `clock_gettime()` should work just fine.
 #include <unistd.h>
 #include <time.h>
 
-#define number_iter 1000000
+#define number_iter 10000
 #define BILLION 1000000000L
 
 int main()
 {
     // Your code here
-    
+    long start_t, end_t, total_t, avg_t;
+    struct timespec start, end;
+    start_t = clock();
+    // clock_gettime(CLOCK_REALTIME)
+    for (int i = 0; i < number_iter; i++)
+    {
+        fprintf(stdout, "."); // empty write to standard out (equivalent to printf(""))
+    }
+    end_t = clock();
+    total_t = (double)(end_t - start_t) / CLOCKS_PER_SEC;
+    avg_t = total_t/number_iter;
+    printf("Total Time and Average time taken by CPU for system call is %f and %lf\n", total_t, avg_t);
     return 0;
 }
