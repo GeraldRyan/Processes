c7b69ec8a4b48955cf78d1e08081318d925ff827
GeraldRyan <gerald.ryan40@yahoo.com>
2020-12-04 22:38:06 -0600

    exercise 5 completed, piping in C

--- ex5/ex5.c
+++ ex5/ex5.c
@@ -1,7 +1,7 @@
 // Write a program that forks a child and creates a shared pipe
-// between the parent and child processes. Have the child write 
-// the three messages to the parent and have the parent print out 
-// the messages. 
+// between the parent and child processes. Have the child write
+// the three messages to the parent and have the parent print out
+// the messages.
 
 #include <stdio.h>
 #include <unistd.h>
@@ -10,13 +10,36 @@
 
 #define MSGSIZE 16
 
-char* msg1 = "hello world #1";
-char* msg2 = "hello world #2";
-char* msg3 = "hello world #3";
+char *msg1 = "hello world #1";
+char *msg2 = "hello world #2";
+char *msg3 = "hello world #3";
 
 int main(void)
 {
     // Your code here
-    
+    int p[2], pid, nbytes;
+    char inbuffer[16];
+    if (pipe(p) < 0)
+    {
+        exit(1);
+    }
+    pid = fork();
+    if (pid > 0)
+    {
+        wait(NULL); // waits till child is done (below)
+        for (int i = 0; i < 3; i++)
+        {
+            read(p[0], inbuffer, MSGSIZE);
+            printf("Message %i: ", i+1);
+            printf("%s\n", inbuffer);
+        }
+    }
+    else
+    { /// child
+        write(p[1], msg1, MSGSIZE);
+        write(p[1], msg3, MSGSIZE);
+        write(p[1], msg2, MSGSIZE);
+    }
+
     return 0;
 }
