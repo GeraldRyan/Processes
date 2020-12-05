e16bbc27f0d175eb228808388070e7c8e41dd5b4
GeraldRyan <gerald.ryan40@yahoo.com>
2020-12-04 22:18:13 -0600

    completed exercise 4, learned how to spawn new process

--- ex4/ex4.c
+++ ex4/ex4.c
@@ -1,6 +1,6 @@
 // Write a program that calls `fork()` and then calls some form of `exec()`
-// to run the program `/bin/ls`. Try a few variants of `exec()`, such as 
-// `execl()`, `execle()`, `execv()`, and others. Why do you think there 
+// to run the program `/bin/ls`. Try a few variants of `exec()`, such as
+// `execl()`, `execle()`, `execv()`, and others. Why do you think there
 // are so many variants of the same basic call?
 
 #include <stdio.h>
@@ -10,7 +10,15 @@
 
 int main(void)
 {
-    // Your code here    
-
+    // Your code here
+    int rc = fork();
+    if (rc == 0)
+    { // it's a child
+        char *args[] = {"/bin/ls", NULL};
+        execvp(args[0], args);
+    }
+    else {
+        // printf("I'm the parent around here. I started things.\n");
+    }
     return 0;
 }
